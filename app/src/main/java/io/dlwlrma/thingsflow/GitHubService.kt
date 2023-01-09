afcf783faeb10936478f83be3a68c428f08ab812
dlwlrma-io <io.dlwlrma@gmail.com>
2023-01-09 15:18:45 +0900

    add github service

--- app/src/main/java/io/dlwlrma/thingsflow/GitHubService.kt
+++ app/src/main/java/io/dlwlrma/thingsflow/GitHubService.kt
@@ -1,4 +1,12 @@
 package io.dlwlrma.thingsflow
 
+import io.dlwlrma.thingsflow.model.Issue
+import io.reactivex.rxjava3.core.Single
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface GitHubService {
+
+    @GET("/repos/{org}/{repo}/issues")
+    fun getIssueList(@Path("org") org: String, @Path("repo") repo: String): Single<List<Issue>>
 }
